cdd57bd0fca5293ad73aeb6df00402a323f0032f
Blard <peteromu76@gmail.com>
2024-02-16 04:47:45 +0100

    final edit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
-const cors = require("cors")
+const cors = require("cors");
 
 const connectDB = require("./src/db/connect");
 
@@ -15,9 +15,14 @@ dotenv.config({ path: ".env" });
 
 const app = express();
 
-let corsOptions = { 
-  origin : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'], 
-} 
+let corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:3000",
+    "https://tsa-car-park.vercel.app",
+  ],
+};
 
 //Routers Imports
 const slotRouter = require("./src/routes/slotRouter");
@@ -26,7 +31,7 @@ const slotRouter = require("./src/routes/slotRouter");
 app.use(express.json());
 
 // middlewares
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/slot", slotRouter);
